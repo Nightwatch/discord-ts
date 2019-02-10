33e374fd3839e8008f029dd4672b9060eb3f4999
Tanner Goins <tannergz28@gmail.com>
2019-02-10 01:54:20 -0600

    Recursively search for commands in directory

--- src/model/client.ts
+++ src/model/client.ts
@@ -1,5 +1,7 @@
 import { Client } from 'discord.js'
 import { CommandoClientOptions, Command } from '.'
+import { promises as fs } from 'fs'
+import * as path from 'path'
 
 export class CommandoClient extends Client {
   public readonly options: CommandoClientOptions
@@ -11,20 +13,20 @@ export class CommandoClient extends Client {
     this.options = options
   }
 
-  public registerCommandsIn(paths: string | string[]) {
-    if (typeof paths === 'string') {
-      this.resolveCommand(paths)
-      return
+  public registerCommandsIn(path: string | string[]) {
+    if (typeof path === 'string') {
+      return walk(path).then(files => {
+        this.resolveCommand(path)
+      })
     }
 
-    paths.forEach(path => {
-      this.resolveCommand(path)
+    return path.forEach(async p => {
+      const files = await walk(p)
+      this.resolveCommand(p)
     })
   }
 
-  private resolveCommand(path: string) {
-    const command = require(path) as Command
-
+  public registerCommand(command: Command) {
     if (this.commands.has(command.options.name)) {
       throw new Error(
         `Command '${
@@ -35,4 +37,31 @@ export class CommandoClient extends Client {
 
     this.commands.set(command.options.name, command)
   }
+
+  private resolveCommand(path: string) {
+    try {
+      const command = require(path) as Command
+      this.registerCommand(command)
+    } catch {
+      // swallow
+    }
+  }
+}
+
+// from https://gist.github.com/kethinov/6658166#gistcomment-2733303
+const walk = async (dir: string, fileList: string[] = []) => {
+  const files = await fs.readdir(dir)
+
+  for (const file of files) {
+    const filepath = path.join(dir, file)
+    const stat = await fs.stat(filepath)
+
+    if (stat.isDirectory()) {
+      fileList = await walk(filepath, fileList)
+    } else {
+      fileList.push(file)
+    }
+  }
+
+  return fileList
 }
