e9c5a0ab8a8de5a42ad8c035a934925a9bcb486d
Tanner Goins <tannergz28@gmail.com>
2019-02-14 21:07:17 -0600

    Fix shard error on start

--- src/model/constants.ts
+++ src/model/constants.ts
@@ -1,4 +1,5 @@
-export const DefaultOptions = {
+import { ClientOptions } from 'discord.js'
+export const DefaultOptions: ClientOptions = {
   disableEveryone: false,
   disabledEvents: [],
   fetchAllMembers: false,
@@ -6,29 +7,17 @@ export const DefaultOptions = {
   messageCacheMaxSize: 200,
   messageSweepInterval: 0,
   presence: {},
-  restSweepInterval: 60,
   restTimeOffset: 500,
   restWsBridgeTimeout: 5000,
   retryLimit: 1,
-  shardCount: 1,
-  totalShardCount: 1,
-
   http: {
-    api: 'https://discordapp.com/api',
     cdn: 'https://cdn.discordapp.com',
     invite: 'https://discord.gg',
     version: 7
   },
-
   ws: {
     compress: false,
-    large_threshold: 250,
-    properties: {
-      $browser: 'discord.js',
-      $device: 'discord.js',
-      $os: process.platform
-    },
-    version: 6
+    large_threshold: 250
   }
 }
 
